71c981483362edaac5415e420ff1c29eaf87ebb3
Richard <szrlee@gmail.com>
2018-10-09 18:42:56 +0800

    change dataset

--- tools/pascal_voc_xml2coco_json_converter_cross.py
+++ tools/pascal_voc_xml2coco_json_converter_cross.py
@@ -256,7 +256,7 @@ class voc2coco:
         """
         Convert voc dataset to coco dataset
         """
-        img_sets = ['train', 'val', 'trainval', 'test']
+        img_sets = ['train', 'test']
 
         for img_set in img_sets:
             ids = self._get_indexs_by_image_set(img_set)
