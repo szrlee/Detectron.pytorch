00b6577097fbd55c7148231cf268a00edf729d8d
Richard <szrlee@gmail.com>
2018-10-27 15:58:17 +0800

    test

--- lib/modeling/fast_rcnn_heads.py
+++ lib/modeling/fast_rcnn_heads.py
@@ -134,7 +134,7 @@ def image_level_loss(cls_score, det_score, rois, image_labels_vec, bceloss, box_
             print(f"gt_classes_ind: shape {gt_classes_ind.shape}\n {gt_classes_ind}")
 
             roi_pos_cls_scores = roi_cls_scores[:, gt_classes_ind]
-            max_roi_pos_cls_scores_ind = ind(torch.argmax(roi_pos_cls_scores, dim=0).cpu().numpy())
+            max_roi_pos_cls_scores_ind = ind[torch.argmax(roi_pos_cls_scores, dim=0).cpu().numpy()]
             print(max_roi_pos_cls_scores_ind)
             print(rois[max_roi_pos_cls_scores_ind, :])
 
