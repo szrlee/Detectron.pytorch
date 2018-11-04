c917818195d345c6cbd789d43fe47a283e84614a
szrlee <szrlee@gmail.com>
2018-11-04 17:49:13 +0000

    add script and motidy learning rate for dt-clipart-VOC

---
+++ test_clipart_with_reg_copy_freeze_conv_body.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+dir=Outputs/e2e_faster_rcnn_R-101-FPN_clipart_weakly_with-spatial-reg_copy-cls-to-det_freeze-conv-body/Nov01-04-08-39_ip-172-31-8-158_step/ckpt/
+config=configs/dt/e2e_faster_rcnn_R-101-FPN_clipart_weakly_with-spatial-reg_copy-cls-to-det_freeze-conv-body.yaml
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --dataset clipart --cfg $config --load_ckpt $dir/model_step499.pth --multi-gpu-testing
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --dataset clipart --cfg $config --load_ckpt $dir/model_step999.pth --multi-gpu-testing
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --dataset clipart --cfg $config --load_ckpt $dir/model_step1499.pth --multi-gpu-testing
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --dataset clipart --cfg $config --load_ckpt $dir/model_step1999.pth --multi-gpu-testing
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --dataset clipart --cfg $config --load_ckpt $dir/model_step2499.pth --multi-gpu-testing
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --dataset clipart --cfg $config --load_ckpt $dir/model_step2999.pth --multi-gpu-testing
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --dataset clipart --cfg $config --load_ckpt $dir/model_step3499.pth --multi-gpu-testing
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --dataset clipart --cfg $config --load_ckpt $dir/model_step3999.pth --multi-gpu-testing
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --dataset clipart --cfg $config --load_ckpt $dir/model_step4499.pth --multi-gpu-testing
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --dataset clipart --cfg $config --load_ckpt $dir/model_step4999.pth --multi-gpu-testing
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --dataset clipart --cfg $config --load_ckpt $dir/model_step5499.pth --multi-gpu-testing
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --dataset clipart --cfg $config --load_ckpt $dir/model_step5999.pth --multi-gpu-testing
