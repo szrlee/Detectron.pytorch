59e24700f1c010d3d3f5c8820fbae4f82895362d
Richard <szrlee@gmail.com>
2018-11-08 21:23:40 +0800

    test how much bbox refinement improve the accuracy

--- lib/modeling/fast_rcnn_heads.py
+++ lib/modeling/fast_rcnn_heads.py
@@ -90,6 +90,7 @@ class fast_rcnn_outputs(nn.Module):
         bbox_pred = self.bbox_pred(x)
 
         if not self.weak_supervise:
+            bbox_pred.fill_(0)
             return cls_score, bbox_pred
 
         # weak supervision
