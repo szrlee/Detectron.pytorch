873391e4895b6c855429d82063b4d43f5f36ba8b
Richard <szrlee@gmail.com>
2018-11-12 07:50:43 +0800

    fixes

--- lib/modeling/fast_rcnn_heads.py
+++ lib/modeling/fast_rcnn_heads.py
@@ -171,8 +171,8 @@ def s1_image_level_loss(cls_score, rois, image_labels_vec, MLSoftMarginLoss, box
             # divided by number of non-background classes
             reg = reg + torch.sum(weighted_feat_dis) / image_labels.shape[1]
         cls_prob = torch.logsumexp(softmax_cls, dim=0)
-        print(f"softmax_cls shape: {softmax_cls.shape}\n {softmax_cls}")
-        print(f"softmax_cls sum over dim 1 shape: {softmax_cls.sum(dim=1)}")
+        # print(f"softmax_cls shape: {softmax_cls.shape}\n {softmax_cls}")
+        # print(f"softmax_cls sum over dim 1 shape: {softmax_cls.sum(dim=1)}")
         print(f"cls_prob shape: {cls_prob.shape}\n {cls_prob}")
         if cls_probs is None:
             cls_probs = cls_prob.unsqueeze(dim=0)
@@ -182,7 +182,7 @@ def s1_image_level_loss(cls_score, rois, image_labels_vec, MLSoftMarginLoss, box
     # spatial regularization 
     # divided by number of images
     reg = reg / image_labels.shape[0]
-    loss_cls = bceloss(cls_probs.clamp(0,1), image_labels)
+    loss_cls = MLSoftMarginLoss(cls_probs.clamp(0,1), image_labels)
     acc_score = cls_probs.round().eq(image_labels).float().mean()
     return loss_cls, acc_score, reg
 
