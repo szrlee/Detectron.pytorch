852a3080ea07b5a4602a3d0b01144ff656326c71
Richard <szrlee@gmail.com>
2018-11-12 12:06:23 +0800

    minor fixes (remove print)

--- lib/modeling/fast_rcnn_heads.py
+++ lib/modeling/fast_rcnn_heads.py
@@ -176,7 +176,7 @@ def s1_image_level_loss(cls_score, rois, image_labels_vec, MLSoftMarginLoss, BCE
         # cls_prob = torch.logsumexp(softmax_cls, dim=0)
         # print(f"softmax_cls shape: {softmax_cls.shape}\n {softmax_cls}")
         # print(f"softmax_cls sum over dim 1 shape: {softmax_cls.sum(dim=1)}")
-        print(f"cls_prob shape: {cls_prob.shape}\n {cls_prob}")
+        # print(f"cls_prob shape: {cls_prob.shape}\n {cls_prob}")
         if cls_probs is None:
             cls_probs = cls_prob.unsqueeze(dim=0)
         else:
