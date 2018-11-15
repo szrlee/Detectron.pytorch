f02e7f47d19ba52c6d5677345390a630a3872870
szrlee <szrlee@gmail.com>
2018-11-15 13:30:10 +0000

    add script

---
+++ scripts/joint_train_dt-clipart-voc_clipart_from-baseline.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pretrain=Outputs/e2e_faster_rcnn_R-101-FPN_1x-VOC/Oct08-14-54-41_ip-172-31-8-158_step/ckpt/model_step9999.pth
+config=configs/joint/e2e_faster_rcnn_R-101-FPN_JointTrain_dt-clipart-voc_clipart_pseudo.yaml
+dataset=dt_clipart_voc0712+clipart
+python tools/train_net_step.py --cfg $config --load_ckpt $pretrain --use_tfboard --dataset $dataset --bs 32 2>&1|tee log/joint_train_dt-clipart-voc_clipart.from_baseline.4e-5.1115.log
