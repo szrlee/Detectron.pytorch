2e8623ef3140aa0145f37df4062f96751ccdbc1f
szrlee <szrlee@gmail.com>
2018-11-17 01:32:21 +0000

    add script and log

---
+++ scripts/train_1s_from_baseline.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+dir=Outputs/e2e_faster_rcnn_R-101-FPN_1x-VOC/Oct08-14-54-41_ip-172-31-8-158_step/ckpt
+
+python tools/train_net_step.py --cfg configs/dt/e2e_faster_rcnn_R-101-FPN_clipart_weakly_1stream_noreg_freeze-boxhead_unlock-rpn-conv.yaml --load_ckpt $dir/model_step9999.pth --use_tfboard --dataset clipart --bs 32 2>&1|tee log/train_1stream_noreg_freeze-boxhead_unlock-rpn-conv.from_baseline.2e-5.1117.log
+
+# python tools/train_net_step.py --cfg configs/dt/e2e_faster_rcnn_R-101-FPN_clipart_weakly_1stream_noreg_freeze-boxhead-conv.yaml --load_ckpt $dir/model_step9999.pth --use_tfboard --dataset clipart --bs 32 2>&1|tee log/train_1stream_noreg_freeze-boxhead-conv.from_dt.output.2e-5.1114.log
